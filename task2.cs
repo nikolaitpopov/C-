9a96d10c14f545dc8a651a2a9bcd646c2a2ff0f9
Nikolai Popov <nikolaitpopov@gmail.com>
2023-07-05 23:05:11 +0300

    task2

---
+++ task2.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+int b = int.Parse(Console.ReadLine());
+if (a>b){Console.WriteLine("Max: "+ a);}
+else {Console.WriteLine("Max: '"+b);}
